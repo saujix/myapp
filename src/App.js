1b2c83c4a1bdbc24e0244525a9e1e89ab3c8b1ac
saujix <chutkipasbola@gmail.com>
2024-11-10 23:56:57 +0000

    alright

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Updater(){
+    return <div>Hello</div>
+};
+
+export function Button(){
+    return <div><button>Hi there</button></div>
+};
+
+export function Time(){
+    const [time, setTime] = useState(Date());
+    useEffect(() => {
+        const  interval = setInterval(()=>{
+            const date = new Date();
+            const showTime = date.getHours() 
+                + ':' + date.getMinutes() 
+                + ":" + date.getSeconds(); 
+            setTime(showTime);
+        },1000)
+    },[]);
+    return <div className='time1'>{time}</div>
 }
 
-export default App;
+
+export function List() {
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            // Your code here when Enter is pressed
+            addItem();
+        }
+    });
+
+    let [arrayList, setArrayList] = useState([]);
+
+    const addItem = () => {
+        const value = document.querySelector(".valuefield").value;
+        const newObject = { value };
+        setArrayList([...arrayList, newObject]);
+             
+        document.querySelector('.valuefield').value ='';
+    };
+
+    let text;
+    return (
+        <div>
+            <div className="listContainer">
+                {arrayList.map((item, index) => (
+                        <div className='containerList' id={`item-${index}`} key={index}>
+                            <input className='value' placeholder={item.value}></input>
+                            <button className='remove' onClick={() => {setArrayList(arrayList = arrayList.splice(0, index).concat(arrayList.splice(index + 1))) }}>Delete</button>
+                        </div>
+                ))}
+            </div>
+
+            <div className='bottom'>
+                <div className='enterValue'>
+                    <input className="valuefield" type='text' placeholder={text}></input>
+                </div>
+                <div className='enter' onClick={addItem}>
+                    Add
+                </div>
+               
+            </div>
+        </div>
+    );
+
+    
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Updater from './App';
+import {Button} from './App';
+import {Time} from './App';
+import './App';
+import "./Index.css";
+import next from './next.png';
+import {List} from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));   //holds the main dom element and is the only one
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <>
+    <div className='container'>
+        <div className='todo'>
+            <div className='top'>
+                <div className='t1'>
+                    <div><h3>TODO</h3></div>
+                </div>
+                <div className='time'>
+                    <Time />
+                </div>
+            </div>
+            <div className='main'>
+                <div className="mainBody">
+                <div className='list'>
+                   <List />
+                    </div>
+                </div>
+            </div>
+        </div>    
+    </div>
+    </>
+   
+)
